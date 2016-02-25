453821824a1ccec03b5d9644923843fcb92e87a0
sujiehua <susan@Susan-Work-Pro.local>
2016-02-25 19:54:43 +0800

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'eval',
+	entry: [
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		'./index'
+	],
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loaders: ['react-hot', 'babel'],
+			exclude: /node_modules/,
+			include: __dirname
+		}]
+	}
+};
