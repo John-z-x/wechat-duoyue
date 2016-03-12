bb94e4a42346f473f0f9f9d80e3f94dcdbfa1f40
Pancheng <772045739@qq.com>
2016-03-12 15:42:19 +0800

    Finished goodslist page

---
+++ components/GoodsList/GoodsListTitle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GoodsListTitle extends React.Component {
+  render() {
+    return (
+      <div className="goods-list-title">
+        <span className="title-image"></span>
+        <span className="title-name">搜索结果:</span>
+      </div>
+    );
+  }
+}
+
+
+export default GoodsListTitle;
