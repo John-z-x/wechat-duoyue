a37b47dc861386aba205193ff6fde77213e05005
fangyuan <yuaneacho@gmail.com>
2016-04-06 21:01:47 +0800

    userMenu-scss

--- src/components/PageUserMenu/ToSocialArea.js
+++ src/components/PageUserMenu/ToSocialArea.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router';
 class ToSocialArea extends React.Component {
   render() {
     return (
-        <div className="ToSocialArea menu-bottom">
+        <div className="ToSocialArea">
           <div className="menu-box">
             <Link to="/socialarea" className="link">
-              <div className="menu-content">
+              <div className="menu-content clearfix">
                 <div className="shequ"></div>
                 <div className="menu-bottom-td">社区
                   <span className="menu-title">{'  '}志同道合/学习交友</span>
--- src/components/PageUserMenu/UserInfo.js
+++ src/components/PageUserMenu/UserInfo.js
@@ -5,33 +5,29 @@ class UserInfo extends React.Component {
   render() {
     let {data} = this.props;
     return (
-        <div className='UserInfo clearfix user-new'>
-
+        <div className='UserInfo clearfix '>
           <div className='left user-new-img'>
             <img src={data.src}/>
             <div className='user-name'>{data.name}</div>
           </div>
-
           <div className="right user-table">
-            <div style={{width: "100%", border: "0"}}>
-              <div className="user-table-td">
-                <Link to="/favorites">
-                  <div>6</div>
-                  <div className="word">收藏</div>
-                </Link>
-              </div>
-              <div className="user-table-td border-aside">
-                <Link to="/userintegration">
-                  <div>5</div>
-                  <div className="word">积分</div>
-                </Link>
-              </div>
-              <div className="user-table-td">
-                <Link to="/orderlist">
-                  <div>7</div>
-                  <div className="word">订单</div>
-                </Link>
-              </div>
+            <div className="user-table-td">
+              <Link to="/favorites">
+                <div>6</div>
+                <div className="word">收藏</div>
+              </Link>
+            </div>
+            <div className="user-table-td border-aside">
+              <Link to="/userintegration">
+                <div>5</div>
+                <div className="word">积分</div>
+              </Link>
+            </div>
+            <div className="user-table-td">
+              <Link to="/orderlist">
+                <div>7</div>
+                <div className="word">订单</div>
+              </Link>
             </div>
           </div>
         </div>
